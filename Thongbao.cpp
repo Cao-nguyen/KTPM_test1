4256d60be8030a9316eacdb9b8b7b0863a94c7ea
Cao-nguyen <caonguyentran1011@gmail.com>
2023-10-23 14:36:28 +0700

    branch_75 thay doi file thongbao.cpp

--- Thongbao.cpp
+++ Thongbao.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 int main(){
 cout<<"Push Thongbao.cpp to repo"<<endl;
-cout<<"84 chinh sua file Thongbao.cpp"<<endl;
+cout<<"75 chinh sua file Thongbao.cpp de conflict"<<endl;
 return 0;
 }
